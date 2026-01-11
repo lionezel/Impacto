1311258c50182b2849268212bd5ce3c4f38923b3
Sofi <sofiasolano@MacBook-Pro-de-Sofi.local>
2026-01-11 11:20:58 -0500

    fix form

--- src/hook/useCheckoutForm.ts
+++ src/hook/useCheckoutForm.ts
@@ -2,63 +2,64 @@ import { useEffect, useState } from "react";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useAuth } from "./useAuth";
 import { db } from "../firebase/config";
+import { RestaurantId } from "../global/restaurantId";
 
 
 export interface CheckoutForm {
-  name: string;
-  address: string;
-  city: string;
-  phone: string;
+    name: string;
+    address: string;
+    city: string;
+    phone: string;
 }
 
 const emptyForm: CheckoutForm = {
-  name: "",
-  address: "",
-  city: "",
-  phone: "",
+    name: "",
+    address: "",
+    city: "",
+    phone: "",
 };
 
 export const useCheckoutForm = () => {
-  const { user } = useAuth(); // user.uid
-  const [form, setForm] = useState<CheckoutForm>(emptyForm);
-  const [loading, setLoading] = useState(true);
+    const { user } = useAuth(); // user.uid
+    const [form, setForm] = useState<CheckoutForm>(emptyForm);
+    const [loading, setLoading] = useState(true);
 
-  // 🔹 Cargar datos al iniciar
-  useEffect(() => {
-    if (!user) return;
+    // 🔹 Cargar datos al iniciar
+    useEffect(() => {
+        if (!user) return;
 
-    const loadForm = async () => {
-      const ref = doc(db, "users", user.uid);
-      const snap = await getDoc(ref);
+        const loadForm = async () => {
+            const ref = doc(db, "restaurants", RestaurantId, "users", user.uid);
+            const snap = await getDoc(ref);
 
-      if (snap.exists() && snap.data()?.checkout) {
-        setForm(snap.data().checkout);
-      } else {
-        setForm(emptyForm); // usuario nuevo
-      }
+            if (snap.exists() && snap.data()?.checkout) {
+                setForm(snap.data().checkout);
+            } else {
+                setForm(emptyForm); // usuario nuevo
+            }
 
-      setLoading(false);
+            setLoading(false);
+        };
+
+        loadForm();
+    }, [user]);
+
+    // 🔹 Guardar cambios
+    const saveForm = async (data: CheckoutForm) => {
+        if (!user) return;
+
+        const ref = doc(db, "users", user.uid);
+        await setDoc(
+            ref,
+            { checkout: data },
+            { merge: true } // 🔥 clave para actualizar sin borrar
+        );
     };
 
-    loadForm();
-  }, [user]);
-
-  // 🔹 Guardar cambios
-  const saveForm = async (data: CheckoutForm) => {
-    if (!user) return;
-
-    const ref = doc(db, "users", user.uid);
-    await setDoc(
-      ref,
-      { checkout: data },
-      { merge: true } // 🔥 clave para actualizar sin borrar
-    );
-  };
-
-  return {
-    form,
-    setForm,
-    saveForm,
-    loading,
-  };
+    return {
+        form,
+        setForm,
+        saveForm,
+        loading,
+    };
 };
